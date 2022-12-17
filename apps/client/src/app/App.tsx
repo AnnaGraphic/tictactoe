7eafe8da0e5eab4e36bd8e6a149b1693adda79f7
annar <p4nd4@posteo.de>
2022-12-17 15:32:16 +0100

    add navbar and Display

--- apps/client/src/app/App.tsx
+++ apps/client/src/app/App.tsx
@@ -1,18 +1,29 @@
 import styles from './app.module.css';
-import { Board } from './board/Board';
 import { Route, Routes, Link } from 'react-router-dom';
 import { useState } from 'react';
+import { Board } from './board/Board';
+import { Display } from './display/Display';
 
 export function App() {
   const [player, setPlayer] = useState('X');
+  const [win, setWin] = useState(null);
+
   return (
     <>
-      <div>
+      <div className="app">
         <div className="navbar">
-          <h1>Tic Tac Toe</h1>
+          <div className="headline">
+            <p>just another </p> <h1 className="headline">Tic Tac Toe</h1>
+          </div>
         </div>
         <div className="game">
-          <Board player={player} setPlayer={setPlayer} />
+          <Display player={player} win={win} />
+          <Board
+            player={player}
+            setPlayer={setPlayer}
+            setWin={setWin}
+            win={win}
+          />
           {/* START: routes */}
           {/* END: routes */}
         </div>
