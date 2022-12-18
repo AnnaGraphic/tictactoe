be359e912e1c7697d9610b97cff8b92c50749822
annar <p4nd4@posteo.de>
2022-12-18 12:58:32 +0100

    Square component renders  hartcoded avatars

--- apps/client/src/app/square/Square.tsx
+++ apps/client/src/app/square/Square.tsx
@@ -2,9 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 export function Square(props) {
-  return (
-    <button className="square" onClick={props.click}>
-      {props.fill}
-    </button>
-  );
+  if (props.fill === 'O') {
+    return (
+      <button className="square" onClick={props.click}>
+        <img
+          className="avatar"
+          src={'/assets/avatar-cat-192x192_1.png' || 'O'}
+          alt="O"
+        />
+      </button>
+    );
+  } else if (props.fill === 'X') {
+    return (
+      <button className="square" onClick={props.click}>
+        <img
+          className="avatar"
+          src={'/assets/avatar-hart-192x192_1.png' || 'X'}
+          alt="X"
+        />
+      </button>
+    );
+  } else {
+    return (
+      <button className="square" onClick={props.click}>
+        {props.fill}
+      </button>
+    );
+  }
 }
