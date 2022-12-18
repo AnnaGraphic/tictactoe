c94b1299a74a88abc8db902b22083cd0de897de3
annar <p4nd4@posteo.de>
2022-12-18 13:00:24 +0100

    add secrets, route for assets

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,29 +1,38 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import * as express from 'express';
 import * as path from 'path';
-import { Hallo } from 'tictactoe-typings';
+//import { Hallo } from 'tictactoe-typings';
 import * as cors from 'cors';
+
 const { WEBSITE } = process.env;
-
 const app = express();
-
-const test: Hallo = {
-  gruss: 'hallo',
-};
-console.log(test);
+// const test: Hallo = {
+//   gruss: 'hallo',
+// };
+// console.log(test);
 
 // +++++++ middleware +++++++
 app.use(cors({ origin: WEBSITE }));
 
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
-// +++++++ route +++++++
+app.use((req, res, next) => {
+  console.log('---------------------');
+  console.log('req.url:', req.url);
+  console.log('req.method:', req.method);
+  // console.log('req.session:', req.session);
+  // console.log('req.session.userId:', req.session.userId);
+  console.log('---------------------');
+  next();
+});
+
+// +++++++ routes +++++++
 app.get('/api', (req, res) => {
   res.send({ message: 'Welcome to server!' });
+});
+
+// +++++++ all routes +++++++
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'assets', '..', 'client', 'public'));
 });
 
 const port = process.env.port || 3333;
