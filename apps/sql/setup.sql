c8a2041e761e1a58281a064d8495ffff22635acd
annar <p4nd4@posteo.de>
2022-12-19 17:58:28 +0100

    changed game table

--- apps/sql/setup.sql
+++ apps/sql/setup.sql
@@ -4,10 +4,8 @@ CREATE TABLE games (
     id SERIAL PRIMARY KEY,
     user_x VARCHAR(255),
     user_x_avatar VARCHAR(255)
-    user_x_game INTEGER,
-    -- 1, 2, 3 | win lose tie
     user_o VARCHAR(255),
     user_o_avatar VARCHAR(255)
-    user_o_game VARCHAR(1),
+    win VARCHAR(1),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
