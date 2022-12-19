69a29f616ab3f1abf0a2eb20226ea8e79c0bf227
annar <p4nd4@posteo.de>
2022-12-19 17:58:59 +0100

    switch to async await

--- apps/client/src/app/submitButton/SubmitButton.jsx
+++ apps/client/src/app/submitButton/SubmitButton.jsx
@@ -1,33 +1,31 @@
 export function SubmitButton({ route, payload, onSuccess, onError, text }) {
-  function handleSubmit({ route, payload, onSuccess, onError }) {
+  async function handleSubmit({ route, payload, onSuccess, onError }) {
     //console.log("About to submit the form!");
     console.log('payload', payload);
     const host = 'http://localhost:3333';
-    fetch(host + route, {
-      method: 'POST',
-      // to send json body in a post, headers is required
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(payload),
-    })
-      .then((res) => {
-        res.json();
+    try{ 
+      const res = await fetch(host + route, {
+        method: 'POST',
+        // to send json body in a post, headers is required
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(payload),
       })
-      .then((response) => {
-        console.log('response handle submit', response);
-        if (response.success) {
-          //"success true"
-          onSuccess(response);
-        } else {
-          console.log('SubmitButton err: ', response);
-          //"success false"
-          onError();
-        }
-      })
-      .catch((err) => {
-        console.log('SubmitButton err: ', err);
-      });
+      const response = await res.json();
+       if (response.success) {
+            //"success true"
+            onSuccess(response);
+          } else {
+            console.log('SubmitButton err: ', response);
+            //"success false"
+            onError();
+          }
+
+    } catch (err) {
+      onError(err)
+    } 
+  
   }
   return (
     <button
