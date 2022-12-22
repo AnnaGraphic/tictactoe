bd592b29001b3801fb0331810b9dd874450e2ad8
annar <p4nd4@posteo.de>
2022-12-22 14:20:38 +0100

    displays alternative character

--- apps/client/src/app/display/DisplaPlayerO.tsx
+++ apps/client/src/app/display/DisplaPlayerO.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 export function DisplayPlayerO(props) {
-    //console.log("props in display o", props, props.game)
+  console.log("props.game, props.player_x_avatar,props.player_o_avatar", props.game, props.game.user_x_avatar, props.game.user_o_avatar)
 
+    if (props.game.user_x_avatar !==  props.game.user_o_avatar){
 return (
     <div className="displayPlayers">
         <img
@@ -11,7 +12,16 @@ return (
               src={`/assets/avatar-${props.game.user_o_avatar}-big.png`}
               alt="player o"
             />
+    </div> )
+ } else {
+return (
+    <div className="displayPlayers">
+        <img
+              className="floating"
+              src={`/assets/avatar-${props.game.user_o_avatar}-big_2.png`}
+              alt="player o"
+            />
     </div>
-)
-
+    )
+}
 }
