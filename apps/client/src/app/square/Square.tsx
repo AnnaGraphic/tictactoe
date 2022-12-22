e5165bac48d1c145c997fd73ee73dc80e3bd1513
annar <p4nd4@posteo.de>
2022-12-22 14:18:52 +0100

    renders alternative icon

--- apps/client/src/app/square/Square.tsx
+++ apps/client/src/app/square/Square.tsx
@@ -2,7 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 export function Square(props) {
-  if (props.fill === 'O') {
+
+ // console.log("square ", props.thisGame)
+  
+ if (props.fill === 'O' && props.game.user_o_avatar === props.game.user_x_avatar) {
+    return (
+      <button className="square" onClick={props.click}>
+        <img
+          className="avatar"
+          src={`/assets/avatar-${props.game.user_o_avatar}-192x192_2.png` || 'O'}
+          alt="O"
+        />
+      </button>
+    );
+  } 
+ else if (props.fill === 'O') {
     return (
       <button className="square" onClick={props.click}>
         <img
@@ -12,7 +26,7 @@ export function Square(props) {
         />
       </button>
     );
-  } else if (props.fill === 'X') {
+     } else if (props.fill === 'X') {
     return (
       <button className="square" onClick={props.click}>
         <img
@@ -22,6 +36,7 @@ export function Square(props) {
         />
       </button>
     );
+    
   } else {
     return (
       <button className="square" onClick={props.click}>
