7673a99377fa84a2bd8b3fbcaaa25c0e0dd35f64
annar <p4nd4@posteo.de>
2022-12-22 12:36:42 +0100

    add audio to start

--- apps/client/src/app/startScreen/StartScreen.tsx
+++ apps/client/src/app/startScreen/StartScreen.tsx
@@ -4,13 +4,23 @@ import { StartButton } from '../button/StartButton';
 import { useNavigate } from "react-router-dom";
 
 export function StartScreen() {
+
+  const audios  = ['../assets/audio/round-1.mp3'];
+
+   let audio = new Audio(audios[Math.floor(Math.random() * audios.length)]);
+   const start = () => {
+    audio.play()
+   }
+  
     const navigate = useNavigate()
     const handleStart = () => {
+         audio.play(), 
       navigate("/profile")
     }
     return (
     <div className="startScreen">
     <StartButton
+        audios={audios}
         handleClick={handleStart}
         text="start"></StartButton>
     </div>
