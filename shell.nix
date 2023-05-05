817efbb7ea41179fa7aec164917fb6f1bcdc5053
panda <p4nd4@posteo.de>
2023-05-05 22:38:53 +0200

    shell.nix: export DATABASE_URL is working now; add source for .env for DB password

--- shell.nix
+++ shell.nix
@@ -9,9 +9,9 @@ pkgs.mkShell {
     pkgs.nodejs 
   ];
   shellHook = ''
+    # DATABASE_PASSWORD must be set in .env
+    . ${lib.escapeShellArg (toString ./.)}/apps/server/.env
     export PATH=$PATH:${lib.escapeShellArg (toString ./.)}/node_modules/.bin
-    export DATABASE_URL=postgresql:///tictactoe
+    export DATABASE_URL=postgresql://"$LOGNAME":"$DATABASE_PASSWORD"@localhost/tictactoe # HACK spiced-pg requires the username followed by a colon and a pwd to not die
   '';
-
-
 }
