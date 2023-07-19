7d225df3f850455081c5dbb1127a88d8d4c400c8
panda <p4nd4@posteo.de>
2023-07-19 20:40:59 +0200

    npm install requires python3

--- shell.nix
+++ shell.nix
@@ -6,8 +6,19 @@ in
 
 pkgs.mkShell {
   nativeBuildInputs = [ 
-    pkgs.nodejs 
+    pkgs.nodejs
+    pkgs.python3
+    (pkgs.writers.writeDashBin "deploy-client" ''
+      # usage: nix-shell --run deploy-client
+      set -efu
+      nx build client
+      rsync -acv dist/apps/client/ shell.c-base.org:public_html/
+      # keep only one css file on server
+      ssh shell.c-base.org ls -t public_html/styles.*.css \| sed 1d \| xargs -r rm -v
+    '')
   ];
+
+
   shellHook = ''
     # DATABASE_PASSWORD must be set in .env
     . ${lib.escapeShellArg (toString ./.)}/apps/server/.env
